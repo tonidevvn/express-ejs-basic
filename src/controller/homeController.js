6ae36dd88d1fde4b63ccaef5311a757788ab910b
PHAM NGUYEN TAN THANG <chipqn@gmail.com>
2023-07-08 22:48:18 +0700

    Connect to DB from homeControler and display on view

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,16 @@
+import connection from "../configs/connectDB";
 let getHomePage = (req, res) => {
-  return res.render("index.ejs");
+  let data = [];
+  // logic
+  // execute will internally call prepare and query
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    console.log(">>> checking mysql2");
+    console.log(results); // results contains rows returned by server
+    data = results.map((row) => {
+      return row;
+    });
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web";
+import connection from "./configs/connectDB";
 
 const app = express();
 const port = process.env.PORT || 3000;
